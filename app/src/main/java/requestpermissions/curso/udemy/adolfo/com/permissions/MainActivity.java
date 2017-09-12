2b5ba976317ad6206ea6fee6d1c9b241adeaed09
Adolfo Chavez <adolfo.dev1295@gmail.com>
2017-09-12 11:41:13 -0500

    cambio en codigo permissions
    se cambio el codigo para añadir comentarios

--- app/src/main/java/requestpermissions/curso/udemy/adolfo/com/permissions/MainActivity.java
+++ app/src/main/java/requestpermissions/curso/udemy/adolfo/com/permissions/MainActivity.java
@@ -47,27 +47,8 @@ public class MainActivity extends AppCompatActivity {
                 && ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_CONTACTS)&& ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.CAMERA)) {
-
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_CONTACTS,Manifest.permission.CAMERA},1);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS,Manifest.permission.CAMERA},1);
         }
 
     }
