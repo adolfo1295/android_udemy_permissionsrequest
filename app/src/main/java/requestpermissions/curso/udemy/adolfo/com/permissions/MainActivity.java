63c7853dc33662d718c2a2031f7ae24e3d9bdf80
Adolfo Chavez <adolfo.dev1295@gmail.com>
2017-09-21 11:20:46 -0500

    readme
    readme

--- app/src/main/java/requestpermissions/curso/udemy/adolfo/com/permissions/MainActivity.java
+++ app/src/main/java/requestpermissions/curso/udemy/adolfo/com/permissions/MainActivity.java
@@ -28,6 +28,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private void askPermissionsWithLibrary(){
+        new AskPermission.Builder(this)
+                .setPermissions(Manifest.permission.SEND_SMS, Manifest.permission.ACCESS_FINE_LOCATION)
+                .setCallback(new PermissionCallback() {
+                    @Override
+                    public void onPermissionsGranted(int requestCode) {
+                        Toast.makeText(MainActivity.this, "yeei permisoss otorgados", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionsDenied(int requestCode) {
+                        Toast.makeText(MainActivity.this, "che culo", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setErrorCallback(new ErrorCallback() {
+                    @Override
+                    public void onShowRationalDialog(PermissionInterface permissionInterface, int requestCode) {
+                        permissionInterface.onDialogShown();
+                    }
+
+                    @Override
+                    public void onShowSettings(PermissionInterface permissionInterface, int requestCode) {
+                        permissionInterface.onSettingsShown();
+                    }
+                })
+                .request(10);
+    }
+
 
     private void askPermissionsNormally(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -57,41 +85,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //Metodo que pedira los permisos con una libreria externa
-    //Libreria usada - https://github.com/Kishanjvaghela/Ask-Permission
-    private void askPermissionsWithLibrary(){
-        //se crea un askPermission.Builder, dandole de parametro el contexto de la app
-        new AskPermission.Builder(this)
-                //aqui van los permisos que se quieren solciitar
-                //cabe destacar que deben agregarse primero en manifest.xml
-                .setPermissions(Manifest.permission.SEND_SMS, Manifest.permission.ACCESS_FINE_LOCATION)
-                //aqui se crea el callback para revisar el resultado de los permisos
-                //si fueron otorgados o denegados
-                .setCallback(new PermissionCallback() {
-                    @Override
-                    public void onPermissionsGranted(int requestCode) {
-                        Toast.makeText(MainActivity.this, "yeei permisoss otorgados", Toast.LENGTH_SHORT).show();
-                    }
 
-                    @Override
-                    public void onPermissionsDenied(int requestCode) {
-                        Toast.makeText(MainActivity.this, "che culo", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                //callback en caso de algun error
-                .setErrorCallback(new ErrorCallback() {
-                    @Override
-                    public void onShowRationalDialog(PermissionInterface permissionInterface, int requestCode) {
-                        permissionInterface.onDialogShown();
-                    }
-
-                    @Override
-                    public void onShowSettings(PermissionInterface permissionInterface, int requestCode) {
-                        permissionInterface.onSettingsShown();
-                    }
-                })
-                //se da un requestCode, que este sera la base de los callbacks
-                .request(10);
-    }
 
 }
